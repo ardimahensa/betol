9552e013a8d819c81b8fe13eaceb725395b86433
ardimahensa <ardimahensa11@gmail.com>
2021-12-08 09:19:40 +0700

    buat beranda

---
+++ application/views/beranda.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<style>
+    body{
+        background-color: rgba(23, 41, 69, 1);
+    }
+    h1{
+        color: white;
+        font-size: 100px;
+        margin-top: 160px;
+        font-family: "open sans";
+        font-variant: small-caps;
+    }
+    .saldo{
+	    width: 200px;
+	    height: 200px;
+	    background: white;
+        margin: 10px 10px 10px 640px;
+	    border-radius: 100%;
+        float: left;
+    }
+    div.saldo img{
+        height: 180px;
+        width: 130px;
+        margin-top: 5px;
+    }
+    div.saldo h2{
+        color: white;
+    }
+    .map{
+        width: 200px;
+	    height: 200px;
+	    background: white;
+        margin: 10px 10px 10px 10px;
+	    border-radius: 100%;
+        float: left;
+    }
+    div.map img{
+        height: 150px;
+        width: 150px;
+        margin-top: 25px;
+    }
+    div.map h2{
+        color: white;
+        margin-top: 31px;
+    }
+    .berita{
+        width: 200px;
+	    height: 200px;
+	    background: white;
+        margin: 10px 10px 10px 10px;
+	    border-radius: 100%;
+        float: left;
+    }
+    div.berita img{
+        height: 150px;
+        width: 150px;
+        margin-top: 25px;
+    }
+    div.berita h2{
+        color: white;
+        margin-top: 31px;
+    }   
+</style>
+<body>
+<center>
+    <h1>Be-Toll</h1>
+<a href="">
+    <div class="saldo">
+        <img src="css/dompet.png">
+        <h2>Saldo</h2>
+    </div>
+</a>
+<a href="https://maps.google.com/">
+    <div class="map">
+        <img src="css/maps.png">
+        <h2>Peta</h2>
+    </div>
+</a>
+<a href="">
+    <div class="berita">
+        <img src="css/news.png">
+        <h2>Berita</h2>
+    </div>
+</a>
+</center>
+</body>
+</html>
